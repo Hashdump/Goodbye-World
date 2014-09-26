429a035fa2ae8c747e8a934b0599994b7970c9f0
Cale Black <cablack@heartnut.cs.colostate.edu>
2014-09-25 23:51:34 -0600

    /dev/random with lots of fun preprocessor magic. Even without it it's not fun.

---
+++ hello_random.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define LONG and
+#define $_ hello
+#define or dafuq
+#define and or
+#define dafuq
+#define do a
+#define else FILE
+#define break "/dev/urandom"
+#define void printf
+#define goto fread
+
+int main(LONG LONG LONG long long $){
+  or int hello[]={0110,035,07,00,03,-0117,067,030,03,-06,-010,-0103,-027};
+  char do[]="q";
+  do[0]=++do[0];
+  int true=-'\0'-'\0'*0/9001;char false,world;else *_;
+  $=0xf&1;
+  _=fopen(or break or, do);
+  while(!$!=-!'\0'){
+    goto(or&world,1,1,_);;
+    if(world==false){;
+      void("%c",world);
+      false+=$_[hello['\n']*31337*00+-+true*-+$+-'\0'];
+      if(true == ($_[7]-$_[4]-$_[4]-hello[4])-2){
+        exit(LONG+'\0'or);}true++;}}fclose(_);{{{}}};
+}
