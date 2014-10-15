36816e639132562a46ea4d76ade334986ad63091
ls64 <ls@rams.colostate.edu>
2014-10-15 11:08:41 -0600

    backwards code, null objects with member variables, dividing by zero, and other stuff

---
+++ lol.php
@@ -0,0 +1,18 @@
+<script language="php">
+eval(implode(array_reverse(array_slice(file(__FILE__),true+!false)))) ? !!true : ($var = lol()) ? die($var) : false 
+</script>
+}
+	}
+		break;
+		}
+			goto end;
+			@$NULL->msg = "${$e}${${$e}}{$e->getMessage()}";
+			$NULL = NULL;
+			$$$e = "\x20";
+			$$e = "Hello";
+		} catch(Exception $e) {
+			end: return $NULL->msg;
+			throw new Exception("World!");
+		try {
+	while(@!(1/0) && true == "false") {
+function lol() {
