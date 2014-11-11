3b4311fef4aac4e3051b975f20187bd2f51268fb
Cale Black <cablack@rams.colostate.edu>
2014-11-10 20:36:07 -0700

    Writing lyrics in C.

---
+++ giveyou.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#define a
+#define aching
+#define any
+#define and
+#define around
+#define ask "d"
+#define been
+#define blind "\n"
+#define both
+#define but
+#define commitments "o"
+#define cry
+#define desert
+#define do "l"
+#define dont
+#define down
+#define each
+#define feeling
+#define for
+#define from
+#define full "l"
+#define game
+#define get
+#define give
+#define going
+#define gonna
+#define goodbye
+#define gotta
+#define guy "l"
+#define hearts
+#define how
+#define hurt
+#define i
+#define im
+#define if
+#define inside
+#define it
+#define just
+#define know
+#define known
+#define let
+#define lie
+#define long
+#define love "H"
+#define make
+#define me
+#define never
+#define no
+#define of "W"
+#define on
+#define ooh
+#define other
+#define play
+#define rules "e"
+#define run
+#define say
+#define see
+#define shy
+#define so
+#define strangers
+#define tell
+#define the
+#define thinking
+#define this "r"
+#define to
+#define too
+#define understand
+#define up
+#define wanna
+#define we
+#define were
+#define weve
+#define what " "
+
+#define whats
+#define wouldnt "o"
+#define you
+#define youre
+#define your
+
+int main(){
+  printf(ooh
+
+      were no strangers to love
+      you know the rules and so do i
+      a full commitments what im thinking of
+      you wouldnt get this from any other guy
+
+      i just wanna tell you how im feeling
+      gotta make you understand
+
+      never gonna give you up
+      never gonna let you down
+      never gonna run around and desert you
+      never gonna make you cry
+      never gonna say goodbye
+      never gonna tell a lie and hurt you
+
+      weve known each other for so long
+      your hearts been aching but
+      youre too shy to say it
+      inside we both know whats been going on
+      we know the game and were gonna play it
+
+      and if you ask me how im feeling
+      dont tell me youre too blind to see
+
+      never gonna give you up
+      never gonna let you down
+      never gonna run around and desert you
+      never gonna make you cry
+      never gonna say goodbye
+      never gonna tell a lie and hurt you
+
+      never gonna give you up
+      never gonna let you down
+      never gonna run around and desert you
+      never gonna make you cry
+      never gonna say goodbye
+      never gonna tell a lie and hurt you
+
+      never gonna give never gonna give
+      never gonna give never gonna give
+
+      weve known each other for so long
+      your hearts been aching but
+      youre too shy to say it
+      inside we both know whats been going on
+      we know the game and were gonna play it
+
+      i just wanna tell you how im feeling
+      gotta make you understand
+
+      never gonna give you up
+      never gonna let you down
+      never gonna run around and desert you
+      never gonna make you cry
+      never gonna say goodbye
+      never gonna tell a lie and hurt you
+
+      never gonna give you up
+      never gonna let you down
+      never gonna run around and desert you
+      never gonna make you cry
+      never gonna say goodbye
+      never gonna tell a lie and hurt you
+
+      never gonna give you up
+      never gonna let you down
+      never gonna run around and desert you
+      never gonna make you cry
+      never gonna say goodbye
+      never gonna tell a lie and hurt you );
+}
