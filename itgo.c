f00c3cc65053756615ca8423819a7eaa905677f2
AsynchronousGillz <ganvana@rams.colostate.edu>
2015-10-19 21:46:43 -0600

    Never going to LET IT GO!

---
+++ itgo.c
@@ -0,0 +1,214 @@
+#include <stdio.h>
+
+#define The
+#define snow "H"
+#define glows
+#define white
+#define on
+#define the
+#define mountain "e"
+#define tonight
+#define Not
+#define a
+#define footprint "l"
+#define to
+#define be
+#define seen
+#define A
+#define kingdom "l"
+#define of
+#define isolation
+#define and
+#define it
+#define looks
+#define like
+#define Im
+#define Queen "o"
+#define wind
+#define is
+#define howling
+#define this
+#define swirling " "
+#define storm
+#define inside
+#define Couldnt
+#define keep
+#define in
+#define Heaven
+#define knows
+#define Ive
+#define tried
+#define Dont
+#define let
+#define them
+#define in
+#define dont
+#define see
+#define Be
+#define good
+#define girl
+#define you
+#define always
+#define have
+#define Conceal
+#define feel
+#define know
+#define Well
+#define now
+#define they
+#define Let
+#define go
+#define go
+#define Cant
+#define hold
+#define anymore
+#define Turn
+#define away
+#define slam
+#define door
+#define I
+#define care
+#define what
+#define theyre
+#define going
+#define say
+#define rage
+#define on
+#define cold
+#define never
+#define bothered
+#define me
+#define Its
+#define funny
+#define how
+#define some
+#define distance "W"
+#define Makes
+#define everything
+#define seem
+#define small "o"
+#define And
+#define fears
+#define that
+#define once
+#define controlled "r"
+#define get
+#define at
+#define all
+#define time
+#define can
+#define do
+#define To
+#define test "l"
+#define limits
+#define break
+#define through
+#define No
+#define right
+#define no
+#define wrong
+#define rules
+#define for
+#define me
+#define free
+#define am
+#define one
+#define with
+#define sky
+#define Youll "d"
+#define cry
+#define Here
+#define stand
+#define here
+#define Ill
+#define stay
+#define My
+#define power
+#define flurries
+#define air
+#define into
+#define ground
+#define soul
+#define spiraling
+#define in
+#define frozen
+#define fractals
+#define around
+#define thought
+#define crystallizes "\n"
+#define an
+#define icy
+#define blast
+#define back
+#define past
+#define rise
+#define dawn
+#define That
+#define perfect
+#define gone
+#define In
+#define light
+#define day
+#define anyway
+
+int main(){
+    printf( The snow glows white 
+    on the mountain tonight
+    Not a footprint to be seen
+    A kingdom of isolation
+    and it looks like Im the Queen
+    The wind is howling like this swirling storm inside
+    Couldnt keep it in
+    Heaven knows Ive tried
+
+    Dont let them in
+    dont let them see
+    Be the good girl you always have to be
+    Conceal dont feel
+    dont let them know
+    Well now they know
+
+    Let it go let it go
+    Cant hold it back anymore
+
+    Let it go let it go
+    Turn away and slam the door
+    I dont care
+    what theyre going to say
+    Let the storm rage on
+    The cold never bothered me anyway
+
+    Its funny how some distance
+    Makes everything seem small
+    And the fears that once controlled me
+    Cant get to me at all
+
+    Its time to see what I can do
+    To test the limits and break through
+    No right no wrong no rules for me
+    Im free
+
+    Let it go let it go
+    I am one with the wind and sky
+    Let it go let it go
+    Youll never see me cry
+    Here I stand
+    And here Ill stay
+    Let the storm rage on
+
+    My power flurries through the air into the ground
+    My soul is spiraling in frozen fractals all around
+    And one thought crystallizes like an icy blast
+    Im never going back the past is in the past
+
+    Let it go let it go
+    And Ill rise like the break of dawn
+    Let it go let it go
+    That perfect girl is gone
+    Here I stand
+    In the light of day
+    Let the storm rage on
+
+    The cold never bothered me anyway );
+}
+
