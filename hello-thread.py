13e7616f6de3dcf1d219f84768a1e2603d6143d2
Tyler Schultz <31357851+tyler-schultz@users.noreply.github.com>
2018-08-26 19:15:14 -0600

    Beautified and shrunk code

--- hello-thread.py
+++ hello-thread.py
@@ -1,23 +1,22 @@
 import threading
 import os
 
-def letter(l):
+def d2c(l):
     threading.current_thread()
     os.getpid()
     print(chr(l), end = '')
 
 if __name__ == "__main__":
- 
     os.getpid()
     threading.main_thread()
- 
-    arr = [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33]
-    threads = []
+
+    d = [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33]
+    ts = []
     
-    for x in range(len(arr)):
-        t = threading.Thread(target = letter, args=(arr[x],))
-        threads.append(t)
+    for i in range(len(d)):
+        t = threading.Thread(target = d2c, args = (d[i],))
+        ts.append(t)
         t.start()
         
-    for z in range(len(arr)):
-        threads[z].join()
+    for j in range(len(d)):
+        ts[j].join()
